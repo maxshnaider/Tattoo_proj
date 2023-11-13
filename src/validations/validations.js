0902a0e7e113108ca45891086cd82fe4be737b91
Maksym <maximshnaider@gmail.com>
2023-11-13 13:29:23 +0200

    delete unused

--- src/validations/validations.js
+++ src/validations/validations.js
@@ -4,5 +4,3 @@ export const nameError = 'Please enter a valid name (letters and spaces only)';
 
 export const phoneCheck = /^\d+$/;
 export const phoneError = 'Please enter a valid phone number (digits only)';
-
-export const commentsError = 'Please enter comments'
